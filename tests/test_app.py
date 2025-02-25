eea4a69ba95743d5ae861f6442abd04683059532
PiyushKumar11 <85399697+PiyushKumar11@users.noreply.github.com>
2025-02-25 10:21:04 +0530

    Added flaky test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,16 @@
+import random
 import unittest
+import subprocess
 
 from src.app import app
 from src.config import SQLALCHEMY_DATABASE_URI
 from src.models import User
 from src.service import UserService
 
+
 class TestApp(unittest.TestCase):
     service = UserService()
-    username = "test_app@cloudaeye.com"
+    username = "test_app@.com"
     password = "Admin@1234"
 
     @classmethod
@@ -22,18 +25,35 @@ class TestApp(unittest.TestCase):
             cls.service.delete_user(cls.username)
 
     def test_01_register_user(self):
-        response = self.app.post('/register', json={'username': self.username, 'password': self.password})
+        response = self.app.post(
+            '/register', json={'username': self.username, 'password': self.password})
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json, {"message": "User registered successfully"})
+        self.assertEqual(
+            response.json, {"message": "User registered successfully"})
 
     def test_02_login_user(self):
-        response = self.app.post('/login', json={'username': self.username, 'password': self.password})
+        response = self.app.post(
+            '/login', json={'username': self.username, 'password': self.password})
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', response.json)
 
-    # Can add more tests here...
+    def test_03_flaky_test(self):
+        # This test may fail due to network conditions or timeout
+        delay = random.uniform(0.2, 0.3)
+        try:
+            result = subprocess.run(
+                ['ping', '-c', '1', 'example.com'],
+                capture_output=True,
+                text=True,
+                timeout=delay  # Very short timeout
+            )
+            print(result)
+            self.assertEqual(result.returncode, 0)
+        except subprocess.TimeoutExpired:
+            self.fail("Test failed due to timeout")
+
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
-    test_loader.sortTestMethodsUsing = None  # None to keep the method definition order
+    test_loader.sortTestMethodsUsing = None
     unittest.TextTestRunner().run(test_loader.loadTestsFromTestCase(TestApp))
